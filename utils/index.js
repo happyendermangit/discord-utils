bd15c6ac63a6f4a10d8d4c25674b3200e8305749
= <=>
2024-01-12 18:43:52 +0000

    add index.js

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+const { findStrings } = require('./getStrings.js')
+const { findExperiments } = require('./getExperiments.js')
+const { findRoutes } = require('./getRoutes.js')
+const { findDispatchEventsTypes } = require('./getDispatchEvents.js')
+
+module.exports = {
+    findStrings,
+    findExperiments,
+    findRoutes,
+    findDispatchEventsTypes
+}
